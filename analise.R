bf8f497421a72b7a04739b5ce470d92816f71c31
Thiago Tavares <thiago.tlopess@gmail.com>
2024-07-18 10:22:09 -0300

    att

--- analise.R
+++ analise.R
@@ -1,4 +1,26 @@
 library(tidyverse)
-library(readxl)
-banco <- read_excel("substancias-quimicas-e-radioativas-acima-do-limite-no-brasil-2018-2020 (1).xlsx")
+#library(readxl)
+banco <- readxl::read_excel("substancias-quimicas-e-radioativas-acima-do-limite-no-brasil-2018-2020 (1).xlsx")
 View(banco)
+
+attach(banco)
+
+banco<-data.frame(banco)
+
+banco_mun<- banco |> 
+  group_by(municipio) |> 
+  summarize(substancia = list(na.omit(substancia)))  |> 
+  ungroup()
+  
+
+View(banco_mun)
+
+
+
+
+##---- AGRUPADO POR SUSBSTANCIA
+agrupado_por_substancia <- banco |> 
+  group_by(substancia) |> 
+  summarize(municipio = n()) |> 
+  arrange(desc(municipio))
+print(agrupado_por_substancia)
