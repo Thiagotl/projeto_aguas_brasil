e09de7aa72c196bfa38ffdbe668cc6c44e51525c
Thiago Tavares <thiago.tlopess@gmail.com>
2024-08-23 19:55:21 -0300

    att

--- analise_area.R
+++ analise_area.R
@@ -1,12 +1,10 @@
 library(sidrar)
-#library(DT)
 library(tidyverse)
-
 
 # https://sidra.ibge.gov.br/tabela/5457
 #x <- get_sidra(x = 5457)
 
-#regiao <- c(1,6)
+#regiao <- c(1,6) # TA COM ERRO ESSA ALMA SEBOSA - API HORRIVEL
 
 #ufs <- c(11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 
 #         29, 31, 32, 33, 34, 35, 41, 42, 43, 50, 51, 52, 53)
@@ -60,6 +58,44 @@ tabela_soja_arroz_2018 <- tabela_2018 |>
 View(tabela_soja_arroz_2018)
 
 openxlsx::write.xlsx(tabela_soja_arroz_2018, file = "tabela_soja_arroz_2018.xlsx")
+
+
+#tabela_2018$`Município (Código)`<-as.numeric(tabela_2018$`Município (Código)`)
+
+# ==========
+
+library(readr)
+sisagua_ano2018<- read_csv("sisagua_ano2018_download_2_nov_2021_versao_rodada_30_jan_2022.csv")
+#View(sisagua_ano2018)
+attach(sisagua_ano2018)
+
+sisagua_ano2018$código_ibge <- as.character(sisagua_ano2018$código_ibge)
+
+#tabela_2018$`Município (Código)`<-as.character(tabela_2018$`Município (Código)`)
+
+# tabela_2018 <- tabela_2018 |> 
+#   rename(código_ibge = `Município (Código)`)
+sisagua_ano2018_sul<-sisagua_ano2018 |> 
+  filter(região_geográfica == "SUL")
+
+tabela_soja_arroz_2018$Municipio <- toupper(tabela_soja_arroz_2018$Municipio)
+
+resultado <- tabela_soja_arroz_2018 |> inner_join(sisagua_ano2018_sul, by = c("Municipio" = "município"))
+View(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
