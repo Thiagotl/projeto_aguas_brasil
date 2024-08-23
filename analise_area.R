8cbb346548d52f9dbd20812b6d1acb4a96a86b45
Thiago Tavares <thiago.tlopess@gmail.com>
2024-08-23 18:22:05 -0300

    att

--- analise_area.R
+++ analise_area.R
@@ -6,18 +6,41 @@ library(tidyverse)
 # https://sidra.ibge.gov.br/tabela/5457
 #x <- get_sidra(x = 5457)
 
-regiao <- c(1,5)
+#regiao <- c(1,6)
+
+#ufs <- c(11, 12, 13, 14, 15, 16, 17, 21, 22, 23, 24, 25, 26, 27, 28, 
+#         29, 31, 32, 33, 34, 35, 41, 42, 43, 50, 51, 52, 53)
+
+
+ufs <- c(41,42,43)
 tabela_2018 <- data.frame()
 
-for(i in regiao){
-  x <- get_sidra(x = 5457, # código da tabela no Sidra
-                 geo = 'City', # nível de detalhe espacial da informação
-                 geo.filter = list("Region" = i), #região
-                 variable = 8331, # variável de interesse
-                 period = as.character(2018) # ano dos dados
+# for(i in regiao){
+#   x <- get_sidra(x = 5457, # código da tabela no Sidra
+#                  geo = 'City', # nível de detalhe espacial da informação
+#                  geo.filter = list("Region" = i), #região
+#                  variable = 8331, # variável de interesse
+#                  period = as.character(2018) # ano dos dados
+#   )
+#   tabela_2018 <- rbind(tabela_2018,x)
+# }
+
+
+for(i in ufs) {
+  x <- get_sidra(x = 5457, 
+                 geo = 'City', 
+                 geo.filter = list("State" = i), 
+                 variable = 8331, 
+                 period = as.character(2018)
   )
-  tabela_2018 <- rbind(tabela_2018,x)
+  tabela_2018 <- rbind(tabela_2018, x)
 }
+
+
+# tabela_teste <- tabela_2018 |> 
+#   separate(Município, into = c("Municipio", "Estado"), sep = " - ") 
+#   
+# View(tabela_teste)
 
 #x <- datatable(tabela_2018) - 
 
